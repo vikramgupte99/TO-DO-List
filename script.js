756629d950f9956779e38315a1d779fc29a37fdd
Vikramgupte99 <vikramgupte99@gmail.com>
2026-01-20 20:26:29 -0500

    Function added inside dom.ready func to add new task. console.log to make sure a task is posted in API

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
 
 $(document).ready(function() {
-console.log('DOM ready !')
-})
+console.log('DOM ready !');
+
+//Function to add new task
+$('.addNewTask').click(function () {
+    var newInput = $('.input').val();
+
+    $.ajax({
+        type: 'POST',
+        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=37',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({
+            task: {
+                content: newInput
+            }
+        }),
+        success: function (response) {
+            $('.todoBody').empty();
+
+                $('.todoBody').append(
+                    '<tr class="bg-info mb-1 text-center">' +
+                    '<td class="taskID">' + response.task.id + '</td>' +
+                    '<td>' + response.task.content + '</td>' +
+                    '<td>Active</td>' +
+                    '<td><button class="btn btn-sm btn-warning edit">Mark completed</button></td>' +
+                    '<td><button class="btn btn-sm btn-danger removeTask">REMOVE</button></td>' +
+                    '</tr>'
+                );
+                $('.input').val('');
+                console.log(response);
+        },
+        error: function (request,textStatus,errorMessage) {
+            console.log(errorMessage);
+        }
+    })
+}) //addNewTask
+})//document
